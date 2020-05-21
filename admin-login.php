df6536fbf47779a74461cdc2bd2335d7e0beb862
Kiara Rose <kiararxsemarcelo@gmail.com>
2020-05-21 21:25:14 +0800

    pahabol pt.3!!! sorry XDDD

--- admin-login.php
+++ admin-login.php
@@ -107,7 +107,7 @@ $numOfSlots = 76 - mysqli_num_rows($data);
 						</div>
 						<div class="row mt-3">
 							<div class="col-6">
-								<a href="#" class="text"><small>Forgot password?</small></a>
+								<a href="forgotpassword.php" class="text"><small>Forgot password?</small></a>
 							</div>
 							<div class="col-6 text-right">
 								<a href="#" class="text"><small>Create new account</small></a>
