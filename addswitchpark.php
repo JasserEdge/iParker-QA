0a8152253798430a06d21d0b770fe22a527f4349
Kiara Rose <kiararxsemarcelo@gmail.com>
2020-05-21 15:59:46 +0800

    updates

--- addswitchpark.php
+++ addswitchpark.php
@@ -8,34 +8,137 @@
 		<!-- Navbar primary -->
         <?php include 'nav.php' ?>
 
-        <!-- Toggle buttons -->
-        <div class="d-flex flex-column align-items-center">
-            <div class="mb-3">
-                <small class="text-uppercase font-weight-bold">Toggle buttons</small>
-            </div>
-            <label class="custom-toggle">
-                <input type="checkbox" checked>
-                <span class="custom-toggle-slider rounded-circle"></span>
-            </label>
-            <!--<span class="clearfix"></span>-->
-            <label class="custom-toggle">
-                <input type="checkbox">
-                <span class="custom-toggle-slider rounded-circle"></span>
-            </label>
-            <label class="custom-toggle">
-                <input type="checkbox">
-                <span class="custom-toggle-slider rounded-circle"></span>
-            </label>
-            <label class="custom-toggle">
-                <input type="checkbox">
-                <span class="custom-toggle-slider rounded-circle"></span>
-            </label>
-            <label class="custom-toggle">
-                <input type="checkbox">
-                <span class="custom-toggle-slider rounded-circle"></span>
-            </label>
+        <div class="container mr-auto ml-auto mt-5 mb-6">
+            <h1>Mga Switch</h1>
         </div>
         
+        <!-- Toggle buttons -->
+        <div class="d-flex flex-row justify-content-even">
+
+            <div class="d-flex flex-column align-items-center">
+                <div class="mb-3">
+                    <small class="text-uppercase font-weight-bold">1st Floor</small>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">1</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox" checked>
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">2</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">3</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">4</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">5</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+            </div>
+
+            <div class="d-flex flex-column align-items-center">
+                <div class="mb-3">
+                    <small class="text-uppercase font-weight-bold">2nd Floor</small>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">1</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">2</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">3</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">4</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">5</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+            </div>
+
+            <div class="d-flex flex-column align-items-center">
+                <div class="mb-3">
+                    <small class="text-uppercase font-weight-bold">3rd Floor</small>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">1</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">2</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">3</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">4</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+                <div class="d-flex flex-row">
+                    <small class="text-uppercase font-weight-bold mr-2">5</small>
+                    <label class="custom-toggle">
+                        <input type="checkbox">
+                        <span class="custom-toggle-slider rounded-circle"></span>
+                    </label>
+                </div>
+            </div>
+        </div>
+       
         <!-- Footer -->
 	    <?php include "footer.php"; ?>
 
