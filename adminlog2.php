2202cbfe10de1ac8d154e29cc58aeab1b28efdae
sheila004 <valdez.sheilamae@gmail.com>
2020-05-22 16:12:01 +0800

    table ajax

--- adminlog2.php
+++ adminlog2.php
@@ -52,7 +52,14 @@ include("dbConnection.php");
     <script src="assets-table/assets/js/demo/jquery.sharrre.js"></script>
     <script src="assets-table/assets/js/demo/demo.js"></script>
 
- 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+      $(document).ready(function() {
+        setInterval(function(){
+          $("#data").load("loadData.php")
+          }, 2000);
+      })
+    </script>
 </head>
 <body>
    
@@ -88,21 +95,8 @@ include("dbConnection.php");
       <th data-field="country">Time-out</th>
       <th data-field="city">Date</th>
     </thead>
-    <tbody>
-      <?php 
-      $result = mysqli_query($con,"SELECT * FROM vehicles ORDER BY parking_id DESC");
-      while($row = mysqli_fetch_assoc($result)){
-      			
-      echo " <tr>
-                <td>".$row['parking_id']."</td>
-                <td>".$row['license_plate']."</td>
-                <td>".$row['time_in']."</td>
-                <td>".$row['time_out']."</td>
-                <td>".$row['date']."</td>
-          </tr>";
-      }
-      ?>
-     
+    <tbody id="data">
+      
     </tbody>
           </table>
         </div>
