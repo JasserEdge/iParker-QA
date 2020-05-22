8c210f0e18f8d3c57057f2a96e71bafab4b7ba84
sheila004 <valdez.sheilamae@gmail.com>
2020-05-22 16:34:23 +0800

    Fix index.php

---
+++ getNumOfVehicle.php
@@ -0,0 +1,24 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "softeng-iparker-db";
+//create connection
+	
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//check connection
+	
+if(!$conn){
+	die("Connection Failed:" . mysqli_connect_error());
+}
+	
+//get data
+//get all information from  table
+	
+$query="SELECT * from vehicles";
+//run the query and store data in a variable
+$data = @mysqli_query($conn, $query);
+
+$numOfSlots = 76 - mysqli_num_rows($data);
+echo $numOfSlots;
+?>
