964152a90b8b92fea6ce5b36538c6bffd2ce058a
EdgeJasser <ameerdilangalen@gmail.com>
2020-05-22 15:19:38 +0800

    add data to database

--- addvehicledb.php
+++ addvehicledb.php
@@ -1,18 +1,9 @@
 <?php
-	session_start();
-	$_SESSION['vehicle'] = 0;
-
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$dbname = "softeng-iparker-db";
-
-	$month = date('m');
-	$day = date('d');
-	$year = date('Y');
-
-	$today = $year . '-' . $month . '-' . $day;
-	$time = date("g:i:a");
+	
 	//create connection
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	//check connection
@@ -20,35 +11,18 @@
 		die("Connection Failed:" . mysqli_connect_error());
 	}
 	
-	
-	//get data
-	//get all information from  table
-	if ($_SESSION['exists'] != 1) {
-		$license = $_POST['licensePlate'];
-		$type = $_POST['vehicleType'];
-
-	} else {
-		$license = $_SESSION['checkplate'];	
-		$type =  $_SESSION['checktype'];
-	}
-	
-	
-	//$spot = "none";
-	$timeIn = $time;
-	//$timeOut = "none";
-	$date = $today;
+	$license = $_POST['plateNumber'];
+	$timeIn = $_POST['timeIn'];
+	$date = $_POST['dateToday'];
 	
 	//make sql insert statement
-	$sql="INSERT into vehicles(license_plate, vehicle_type, time_in, date) values ('" . $license . "','" . $type . "','" . $timeIn . "','" . $date . "');" ;
+	$sql="INSERT into vehicles(license_plate, time_in, date) values ('" . $license . "','" . $timeIn . "','" . $date . "');" ;
 	//run the query 
 	if(mysqli_query($conn, $sql)){
-		$_SESSION['vehicle'] = 1;
-		header("Location:parkmanage.php");
+		header("Location:parkingspots.php");
 		exit;
 	} else{
-		$_SESSION['vehicle'] = 2;
-		header("Location:parkmanage.php");
-		exit;
+		echo("Input data is fail");
 	}
 
 	mysqli_close($conn);
